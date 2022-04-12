135b87c71f4602f89801a21f2dacb9db369c4d38
uGuardian <Sean.Heavysege@gmail.com>
2022-04-12 14:18:59 -0600

    Updated optional harmony patches for more efficiency

--- Harmony_Optional.cs
+++ Harmony_Optional.cs
@@ -1,66 +1,103 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using HarmonyLib;
 using UnityEngine;
 using abcdcode_brawl_MOD;
 
-namespace FinallyBeyondTheTime {
+namespace FinallyBeyondTheTime.HarmonyOptional {
 	public class FinallHarmony {
-		public static void Load() {
-			Harmony harmony = new Harmony("LoR.uGuardian.Finnal");
-			FinnalConfig.HarmonyMode = 1;
-			CheckSummonLiberation();
-			harmony.PatchAll();
-		}
-		public static void CheckSummonLiberation() {
-			List<String> assembly = new List<String>();
-			foreach (var a in AppDomain.CurrentDomain.GetAssemblies()) {
-				assembly.Add(a.GetName().Name);
+		readonly Harmony harmony = new Harmony("LoR.uGuardian.Finnal");
+		public void Load() {
+			try {
+				harmony.PatchAll();
+				FinnalConfig.HarmonyMode = 1;
+				if (FinnalConfig.Instance.DiceSpeedUp) {
+					harmony.Patch(typeof(RencounterManager).GetMethod(nameof(RencounterManager.StartRencounter), AccessTools.all),
+						postfix: new HarmonyMethod(typeof(EnableNoDelay).GetMethod(nameof(EnableNoDelay.Postfix))));
+				}
+			} catch (Exception ex) {
+				Debug.LogException(ex);
 			}
+
+			try {
+				CheckOtherMods();
+			} catch (Exception ex) {
+				Debug.LogException(ex);
+			}
+		}
+		public void CheckOtherMods() {
+			List<string> assembly = new List<string>();
+			bool brawlFound = false;
+			foreach (var a in AppDomain.CurrentDomain.GetAssemblies()) {
+				switch (a.GetName().Name) {
+					case "BaseMod" when FinnalConfig.HarmonyMode < 2 && a.GetType("SummonLiberation.Harmony_Patch") != null:
+						UnityEngine.Debug.Log("Finnal: BaseMod SummonLiberation Found");
+						FinnalConfig.HarmonyMode = 2;
+						break;
+					case "abcdcode_brawl_MOD":
+						harmony.Patch(typeof(RencounterManager).GetMethod(nameof(RencounterManager.EndRencounter), AccessTools.all),
+							postfix: new HarmonyMethod(typeof(BrawlModPatch).GetMethod(nameof(BrawlModPatch.Postfix))));
+						brawlFound = true;
+						break;
+				}
+				if (brawlFound && FinnalConfig.HarmonyMode == 2) {
+					break;
+				}
+			}
+			/*
 			if (assembly.Contains("BaseMod")) {
-				UnityEngine.Debug.Log("Finall: BaseMod Found");
+				UnityEngine.Debug.Log("Finnal: BaseMod Found");
 				FinnalConfig.HarmonyMode = 2;
 			}
+			if (AppDomain.CurrentDomain.GetAssemblies().Any(a => a.GetName().Name.Contains("BaseMod")
+				&& a.GetType("SummonLiberation.Harmony_Patch") != null)) {
+					UnityEngine.Debug.Log("Finnal: BaseMod SummonLiberation Found");
+					FinnalConfig.HarmonyMode = 2;
+			}
+			*/
 		}
 	}
 
+	/* Removed this patch because it doesn't quite work properly and it's unneccecary overhead.
 	[HarmonyPatch(typeof(BattleEmotionCoinSlotUI))]
 	class EmotionCoinExceptionSuppressor {
 		[HarmonyFinalizer]
 		[HarmonyPatch("Init")]
-        public static Exception Init_Finalizer(Exception __exception) {
+		public static Exception Init_Finalizer(Exception __exception) {
 			if (Singleton<StageController>.Instance.EnemyStageManager is EnemyTeamStageManager_UltimaAgain) {
-    			return null;
+				return null;
 			}
-            return __exception;
-        }
+			return __exception;
+		}
 		[HarmonyFinalizer]
 		[HarmonyPatch("StartMoving")]
-        public static Exception StartMoving_Finalizer(Exception __exception) {
+		public static Exception StartMoving_Finalizer(Exception __exception) {
 			if (Singleton<StageController>.Instance.EnemyStageManager is EnemyTeamStageManager_UltimaAgain) {
-    			return null;
+				return null;
 			}
-            return __exception;
-        }
+			return __exception;
+		}
 		// [HarmonyFinalizer]
 		// [HarmonyPatch(typeof(BattleEmotionCoinUI), "Init")]
-        // public static Exception Init_Finalizer2(Exception __exception) {
+		// public static Exception Init_Finalizer2(Exception __exception) {
 		// 	if (Singleton<StageController>.Instance.EnemyStageManager is EnemyTeamStageManager_UltimaAgain) {
-    	// 		return null;
+		// 		return null;
 		// 	}
-        //     return __exception;
-        // }
-    }
-	[HarmonyPatch(typeof(RencounterManager), "StartRencounter")]
-    class EnableNoDelay {
-        public static void Postfix(RencounterManager __instance) {
-    		if (FinnalConfig.Instance.DiceSpeedUp && Singleton<StageController>.Instance.EnemyStageManager is EnemyTeamStageManager_UltimaAgain) {
-    			__instance.SetNodelay(true);
-    		}
-    	}
-    }
-	[HarmonyPatch(typeof(RencounterManager), "EndRencounter")]
-	class BrawlModPatch {
+		//	 return __exception;
+		// }
+	}
+	*/
+	// [HarmonyPatch(typeof(RencounterManager), "StartRencounter")]
+	static class EnableNoDelay {
+		public static void Postfix(RencounterManager __instance) {
+			if (Singleton<StageController>.Instance.EnemyStageManager is EnemyTeamStageManager_UltimaAgain) {
+				__instance.SetNodelay(true);
+			}
+		}
+	}
+	// [HarmonyPatch(typeof(RencounterManager), "EndRencounter")]
+	static class BrawlModPatch {
 		public static void Postfix(RencounterManager __instance) {
 			if (Harmony.HasAnyPatches("LOR.abcdcode.Brawl")) {
 				BrawlMod.Fix(__instance);
@@ -75,7 +112,7 @@ namespace FinallyBeyondTheTime {
 		}
 	}
 	[HarmonyPatch(typeof(BattleUnitTargetArrowManagerUI), "ShowTargetLines")]
-	class ShowTargetLinesPatch {
+	static class ShowTargetLinesPatch {
 		// This patch is always applied because when it occurs it's always a bug.
 		public static Exception Finalizer(Exception __exception) {
 			if ((__exception != null) && !(__exception is ArgumentOutOfRangeException)) {
--- Initializer.cs
+++ Initializer.cs
@@ -1,27 +1,33 @@
 using System;
 using System.IO;
 using System.Collections.Generic;
+using System.Security.Permissions;
 using UnityEngine;
 using GameSave;
 using Mod;
 
+#pragma warning disable CS0618
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+#pragma warning restore CS0618
+
 namespace FinallyBeyondTheTime {
-    public class Initializer : ModInitializer {
-        public override void OnInitializeMod() {
-            List<String> assembly = new List<String>();
+	public class Initializer : ModInitializer {
+		public override void OnInitializeMod() {
+			List<string> assembly = new List<string>();
 			foreach (var a in AppDomain.CurrentDomain.GetAssemblies()) {
 				assembly.Add(a.GetName().Name);
 			}
 			#if NoHarmony
 			#warning Compiled with NoHarmony debug option
-			Debug.LogWarning("Finall: Compiled with NoHarmony debug option");
+			Debug.LogWarning("Finnal: Compiled with NoHarmony debug option");
 			#else
 			if (assembly.Contains("0Harmony")) {
-				Debug.Log("Finall: Harmony Found");
-				FinallHarmony.Load();
+				Debug.Log("Finnal: Harmony Found");
+				var harmonyOptional = new HarmonyOptional.FinallHarmony();
+				harmonyOptional.Load();
 			} else {
-				Debug.Log("Finall: Harmony Unavailable");
-            }
+				Debug.Log("Finnal: Harmony Unavailable");
+			}
 			#endif
 
 			// Old to new config migration
@@ -29,7 +35,7 @@ namespace FinallyBeyondTheTime {
 			if (File.Exists(oldConfig)) {
 				Singleton<ModContentManager>.Instance.AddWarningLog("Finnal Battle: Now supports ConfigAPI for in-game settings. It can be found in the workshop.");
 				Directory.CreateDirectory(SaveManager.GetFullPath("ModConfigs"));
-				string newConfig = SaveManager.GetFullPath("ModConfigs\\Finnal.ini");
+				string newConfig = SaveManager.GetFullPath("ModConfigs/Finnal.ini");
 				try {File.Delete(newConfig);} catch {}
 				File.Move(oldConfig, newConfig);
 			}
@@ -43,8 +49,8 @@ namespace FinallyBeyondTheTime {
 				tempInstance.Load();
 				tempInstance.EchoAll();
 			}
-        }
-    }
+		}
+	}
 	internal class InitConfig {
 		internal void Init() {
 			ConfigAPI.Init("Finnal", FinnalConfig.Instance, "HarmonyMode = "+FinnalConfig.HarmonyMode);
